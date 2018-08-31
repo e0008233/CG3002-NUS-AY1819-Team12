c3f3cb66da00d09ab416f80cc3c64e8e5fa3c64a
e0008233 <howard.zhang.1995@gmail.com>
2018-08-31 22:38:38 +0800

    testing numpy
    Signed-off-by: e0008233 <howard.zhang.1995@gmail.com>

--- testing.py
+++ testing.py
@@ -1,1 +1,43 @@
-print ("Hello World")
+from sklearn import datasets
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+#loading the iris dataset
+iris = datasets.load_iris()
+
+x = iris.data #array of the data
+y = iris.target #array of labels (i.e answers) of each data entry
+
+#getting label names i.e the three flower species
+y_names = iris.target_names
+
+#taking random indices to split the dataset into train and test
+test_ids = np.random.permutation(len(x))
+
+#splitting data and labels into train and test
+#keeping last 10 entries for testing, rest for training
+
+foo=75
+x_train = x[test_ids[:-foo]]
+x_test = x[test_ids[-foo:]]
+
+y_train = y[test_ids[:-foo]]
+y_test = y[test_ids[-foo:]]
+
+clf = GaussianNB()
+
+#training (fitting) the classifier with the training set
+clf.fit(x_train, y_train)
+
+#predictions on the test dataset
+pred = clf.predict(x_test)
+
+print(pred) #predicted labels i.e flower species
+print(y_test) #actual labels
+print("Accuracy:", (accuracy_score(pred, y_test))*100) #prediction accuracy
+
+
+#y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
+#print("Number of mislabeled points out of a total %d points : %d"
+#      % (iris.data.shape[0],(iris.target != y_pred).sum()))
