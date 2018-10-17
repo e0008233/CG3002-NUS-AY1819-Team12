7b64b24f2b58c1a513dc334b8b0a7670971f8cbd
Jelena <neohuilingjelena@gmail.com>
2018-10-17 18:40:31 +0800

    refactor

--- Software/prediction.py
+++ Software/prediction.py
@@ -1,5 +1,5 @@
 import numpy as np
-import ProcessData
+import processLiveData
 from keras.models import model_from_json
 from sklearn.externals import joblib
 from keras import optimizers
@@ -8,16 +8,19 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 
 # loading model and encoder
-model = model_from_json(open('model_architecture.json').read())
+with open('model_architecture.json', 'r') as f:
+    loaded_model_json = f.read()
+model = model_from_json(loaded_model_json)
 model.load_weights('model_weights.h5')
 encoder = joblib.load('encoder.sav')
 sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-activity_list = encoder.inverse_transform([0, 1, 2])
+activity_list = encoder.inverse_transform([0, 1, 2, 3, 4])
+print(activity_list)
 
 
 def predict(data):
-    features = ProcessData.extract_features(data)
+    features = processLiveData.extract_features(data)
     mlp_result = model.predict_classes(features)
     rf_result = model.predict(features)
     # find the most common label in result
