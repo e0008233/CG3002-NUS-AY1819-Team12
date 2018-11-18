068dda4925ca221e23f4dbe14fba777e794b9c46
royceljh <31101523+royceljh@users.noreply.github.com>
2018-11-18 21:47:23 +0800

    Added Raspberry Pi main comms code

---
+++ Comms/commsreader.py
@@ -0,0 +1,327 @@
+#!/usr/bin/env python3
+
+##################### Imports #####################
+import datetime, time
+import serial
+import sys, os
+import csv
+import operator
+import collections
+import socket
+from Crypto.Cipher import AES
+from Crypto import Random
+import base64
+
+import prediction
+from sklearn.externals import joblib
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+debug = 0
+##################### Functions #####################
+##### Arduino Handshake #####
+def Handshake_Operation():
+    print('Entering Handshake Mode:')
+    wait_flag = 0
+    while handshake_complete == False:
+        ser.write(handshake)
+        if wait_flag == 0:
+            print('Awaiting response from Arduino...')
+            wait_flag = 1
+        if ser.in_waiting:
+            ser.write(handshake)
+            response = ser.read().decode('utf-8')
+            print('Response: ' + response)
+            if response == 'A':
+                print('Handshake complete')
+                return True
+
+def Msg_Restart():
+    restart_flag = input('Do you want to reset message id? y if you want to, press any other key if you do not: ')
+    if restart_flag == 'y':
+        print('Restarting')
+        ser.write(ackRestart)
+    else: 
+        print('Starting')
+        ser.write(ack)
+
+##### Connect to server #####
+def Connect_Server():
+    global client
+    while (1):
+        print('Connecting to server...')
+        ip_addr = '172.20.10.14'
+        port = 8888
+        address = (ip_addr, port)
+        client = socket.socket()
+        client.connect(address)
+        print('Connected to server')
+        break
+
+##### Check printable ASCII data #####
+def isPrintable(data):
+    for char in data:
+        if (ord(char) <= 31 or ord(char) >= 127):
+            return False
+    return True
+
+### NOT USED ####
+##### Determine final prediction #####
+def finalPrediction(prediction):
+    predict_dict = {'wipers':0, 'number7':0, 'chicken':0, 'sidestep':0, 'turnclap':0, 'cowboy':0, 'salute':0, 'number6':0, 'swing':0, 'mermaid':0}
+    weights = [1,1,2,2,3]
+    for i in range(len(prediction)):
+        if prediction[i] in predict_dict.keys():
+            predict_dict[prediction[i]] += int(weights[i])
+    if max(predict_dict.values()) < 5:
+        return 'confused'
+    else:
+        val = float(max(predict_dict.values())/9*100)
+        print(str(val) + '% confidence')
+        return max(predict_dict.items(), key=operator.itemgetter(1))[0]
+
+##### Power Calculation #####
+def calculatePower(I, V):
+    global cum_power
+    global power_millis
+    power = float(I) * float(V)
+    cum_power += power * float((current_milli_time() - power_millis - 500)/3600)
+    power_millis = current_milli_time()
+    return power
+
+##### AES Encryption #####
+def encryptData(raw):
+    BS = 16
+    padding = ' '
+    pad = lambda s: s + (BS - (len(s) % BS)) * padding
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    encoded_data = base64.b64encode(iv + cipher.encrypt(pad(raw).encode('utf-8')))
+    print (encoded_data)
+    return encoded_data
+
+##################### Variables/Constants Initialization #####################
+handshake_complete = False
+message_id = 0
+handshake = ('H').encode('utf-8')
+ack = ('A').encode('utf-8')
+nack = ('N').encode('utf-8')
+ackRestart = ('K').encode('utf-8')
+samples = []
+startIndex = 0
+endIndex = 72
+incrementAmt = float(endIndex * 0.50)
+currentIndex = 0
+prediction_mlp = []
+prediction_rf = []
+cum_power = 0.0
+
+##### Secret key #####
+key = b'cg3002ispassword'
+client = 0
+
+##################### Main #####################
+print(str(datetime.datetime.now()))
+print('Initiate Connection to Raspberry Pi 3...')
+ser = serial.Serial('/dev/serial0', 230400, timeout=1)
+print('Connected to Raspberry Pi 3')
+if Handshake_Operation():
+    handshake_complete = True
+if debug == 0:
+    Connect_Server()
+
+mode = input('Mode of operation (T for training, L for live): ')
+
+if mode == 'T':
+    # Prepare csv file
+    input_filename = input('Create a new csv file (input d for default name):')
+    if input_filename == 'd':
+         input_filename = 'training_raw.csv'
+    file_action = 'w'
+    while os.path.exists(input_filename):
+        user_input = input(input_filename + ' already exists. Press o to overwrite, a to append, n to choose another filename:')
+        if user_input == 'o':
+            print(input_filename + ' contents will be overwritten.')
+            break
+        elif user_input == 'a':
+            print(input_filename + ' will be appended.')
+            file_action = 'a'
+            break
+        elif user_input == 'n':
+            input_filename = input('Choose another filename:')
+        else:
+            sys.exit('Invalid input, terminating program...')
+    f = open(input_filename, file_action).close()       
+
+    if file_action == 'w':
+        with open(input_filename, file_action) as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['id','acc_rh_x','acc_rh_y','acc_rh_z','gyro_rh_x','gyro_rh_y','gyro_rh_z',
+            'acc_lh_x','acc_lh_y','acc_lh_z','gyro_lh_x','gyro_lh_y','gyro_lh_z',
+            'acc_leg_x','acc_leg_y','acc_leg_z','gyro_leg_x','gryo_leg_y','gryo_leg_z'])
+        print('csv headers initialized')
+
+    csv_file = open(input_filename, 'a')
+
+#ser.write(ack)  # start receiving data from arduino
+Msg_Restart()
+currentTime = datetime.datetime.now()
+message = ser.readline()
+current_milli_time = lambda: int(round(time.time() * 1000))
+previous_millis = current_milli_time()
+count = 0
+freq1_millis = current_milli_time()
+time_previous = current_milli_time()
+power_millis = current_milli_time()
+prediction_count = 0
+dance_count = 0
+ser.flush()
+
+while 1:
+    match_flag = False
+    message = ser.readline().decode()
+    if message == 'A' and message != '':
+        ser.write(ack)
+    elif message != '':
+        previous_millis = current_milli_time()
+        transmitted_checksum = message.split('|')[-1]
+        msg_full = str(message.rsplit('|', 1)[0])
+        msg_checksum = 0
+        if isPrintable(msg_full):
+            for char in msg_full:
+                msg_checksum ^= int(ord(char))
+            if str(transmitted_checksum).strip() == str(msg_checksum).strip():
+                #print("Checksum matches")
+                #print("Message: %s" % msg_full)
+                match_flag = True
+                ser.write(ack)
+                
+                ##### Print Frequency every 100 data received ####
+                ### Used in training mode ###
+                count +=1
+                if count >=100:
+                    time_now = current_milli_time()
+                    count = 0
+                    frequency = 1.0/((float(time_now - time_previous))/100000)
+                    time_previous = current_milli_time()
+                    #print("Frequency: %f" % frequency)
+            else:
+                print('Checksums do not match: %s, %s' % (transmitted_checksum, msg_checksum))
+                print(message)
+                # Resend data
+                ser.write(nack)
+        else: 
+            print('Message contains non-ASCII character(s)')
+            print('Message may be corrupted')   
+
+        if match_flag == True:
+            ##### Retrieve Message ID #####
+            message_id = msg_full.split('|')[0]
+            ##### Prints message every 200 received #####
+            if message_id.isdigit():
+                if (int(message_id) % 200 == 0):
+                    print("Message: %s" % msg_full)
+            
+            ##### Retrieve Current & Voltage #####
+            measurement = msg_full.split('|')[-1]
+            voltage = float(measurement.split(',')[1])
+            current = float(measurement.split(',')[0])
+            #print('Measurements: %s' % measurement)
+
+            ##### Retrieve Sensor Data #####
+            message_content = msg_full.split('|')[1:-1]
+            #print('Message Contents: %s' % message_content)
+
+            ##### Formatting row for csv ######
+            row_content = ''
+            row_content += (message_id + ',')
+            for i in range(1, 4):
+                index = 0
+                # Search for device ID and append data in incremental order
+                while not message_content[index][0] == str(i) and index < len(message_content):
+                    index += 1
+                row_content += (message_content[index][2:] + ',')
+            # Removes last comma
+            row_content = row_content[:-1]
+            #print(row_content)
+            
+
+            ##### Formatting data to fit in buffer for live mode ######
+            sample_content = row_content.split(',')[1:]
+            
+            ##### Training mode #####
+            if mode == 'T':
+                csv_file.write(row_content + '\n')
+                #print('data values written')
+
+            ##### Live mode #####
+            elif mode == 'L':
+                samples.append([])
+                for component in sample_content:
+                    samples[currentIndex].append(component)
+                if len(samples[currentIndex]) != 18:
+                    print('Insufficient data in sample')
+                currentIndex += 1
+
+                ##### Runs prediction when there are enough data #####
+                if len(samples) >= endIndex:
+                    # Check frequency
+                    freq2_millis = current_milli_time()
+                    freq = float(incrementAmt) / float((freq2_millis - freq1_millis)/1000)
+                    print('Frequency: ' + str(freq))
+                    freq1_millis = current_milli_time()
+                    mlp_result = prediction.predict(samples[int(startIndex):int(endIndex)])
+                    ##### Shifts the indexes for the next iteration #####
+                    startIndex += incrementAmt
+                    endIndex += incrementAmt
+
+                    prediction_mlp.append(mlp_result)
+                    if len(prediction_mlp) >= 3:
+                        ##### Checks the last 2 prediction and there are less than 10 extra predictions #####
+                        if prediction_mlp[-1] == prediction_mlp[-2] or prediction_count >= 10:
+                            predicted_move = prediction_mlp[-1]
+
+                            ##### Compute power here #####
+                            power = calculatePower(voltage,current)
+
+                            if predicted_move == 'byebye' and dance_count >= 40:
+                                predicted_move = 'logout'
+                            if predicted_move == 'number6':
+                                predicted_move = 'numbersix'
+                            server_message = '#' + predicted_move + '|' + str(voltage) + '|' + str(current) + '|' + str(power) + '|' + str(cum_power)
+                            
+                            if debug==0:
+                                client.send(encryptData(server_message))
+
+                            ##### Shifts the indexes again to avoid overlapping in the next dance move #####
+                            startIndex += incrementAmt
+                            endIndex += incrementAmt
+
+                            ##### Keeps track of number of dances to avoid sending logout to server preemptively#####
+                            dance_count += 1
+                            print('\n=========================\n Final prediction: ' + predicted_move + '\n=========================\n')
+                            
+                            ##### Clears buffer and goes to sleep #####
+                            prediction_mlp = []
+                            prediction_count = 0
+                            time.sleep(0.5)
+
+                        ##### Else continue getting next prediction #####
+                        else:
+                            print('Confused! getting one more prediction...')
+                            prediction_count += 1
+
+    else:
+        ##### Goes into rehandshake mode if there is no legitimate data received from Arduino #####
+        current_millis = current_milli_time()
+        diff_millis = current_millis - previous_millis
+        if diff_millis > 3000:
+            handshake_complete = False
+            if Handshake_Operation():
+                handshake_complete = True
+                ser.flush()
+                ser.write(ack)
+            
+if mode == 'T':
+    csv_file.close()
+
